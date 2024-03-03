c314c26bc952a2899e3bef21b4882aae2f99533a
Brylle Galquiera <142909052+Brylsmn@users.noreply.github.com>
2024-03-03 11:46:47 +0000

    first commit

---
+++ SuspendedState.java
@@ -0,0 +1,25 @@
+public class SuspendedState implements AccountState{
+    public void deposit(Account account, Double depositAmount){
+        System.out.println("You cannot deposit on suspended account!");
+        System.out.println(account.toString());
+    }
+
+    public void withdraw(Account account, Double depositAmount){
+        System.out.println("You cannot withdraw on suspended account!");
+        System.out.println(account.toString());
+    }
+
+    public void activate(Account account){
+        account.setState(new ActiveState());
+        System.out.println("Account is activated!");
+    }
+
+    public void suspend(Account account){
+        System.out.println("Account is already suspended!");
+    }
+
+    public void close(Account account){
+        account.setState(new ClosedState());
+        System.out.println("Account is closed!");
+    }
+}
